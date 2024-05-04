28907d8678bb3c02e5ffc420f58105bdfb6a7154
shashankdv1 <155424266+shashankdv1@users.noreply.github.com>
2024-05-05 03:07:12 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer()
     return(
         <div className="footer">
             <div className="connect">
-            <h1 id ="about">Connect with us:-</h1><a href="https://www.instagram.com/shashank_vaze/"><FaInstagram style={{background: 'linear-gradient(45deg, #405DE6, #5851DB, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #F77737, #FCAF45)', marginLeft:'10px'}}/> </a><a href="https://www.linkedin.com/in/shashankvaze/"><FaLinkedin  style={{background: 'linear-gradient(45deg, #0077B5, #1B8AAA, #49A7C3, #64B1CC, #7AB6D3)',marginLeft:'10px'}}/></a>
+            <h1 id ="about">Connect with us:-</h1>&nbsp;<a href="https://www.linkedin.com/in/shashankvaze/"><FaLinkedin  style={{background: 'linear-gradient(45deg, #0077B5, #1B8AAA, #49A7C3, #64B1CC, #7AB6D3)',marginLeft:'10px'}}/></a>
             </div>
             <p>© Shashank Vaze, {currentYear}</p>
         </div>
